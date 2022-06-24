d7ba5b82be5a506bd5b593b250c9438b19e82205
Taichi Soma <sosomasox@gmail.com>
2022-06-24 23:54:49 +0900

    環境変数 ENABLE_WEBHOOKSを追加

--- main.go
+++ main.go
@@ -131,10 +131,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&berthv1alpha1.Disk{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "Disk")
-		os.Exit(1)
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = (&berthv1alpha1.Disk{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Disk")
+			os.Exit(1)
+		}
 	}
+
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
